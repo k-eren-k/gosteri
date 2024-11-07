e5276720d9ea73dc7fdac719831c4ab36630f3d3
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-11-07 22:58:28 +0300

    Update song.js

--- models/song.js
+++ models/song.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const songSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  filePath: { type: String, required: true }, // Cloudinary URL'sini saklar
+  artist: { type: String, required: true },
+  year: { type: Number },
+  genre: { type: String },
 });
 
-module.exports = mongoose.model('Song', songSchema);
+const Song = mongoose.model('Song', songSchema);
+module.exports = Song;
