183e66eaa828beaf9e2777ba7010b128a0f80c2d
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-11-07 18:58:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const songRoutes = require('./routes/songs');
+const ejs = require('ejs');
 
 const app = express();
 
