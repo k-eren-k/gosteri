9308a9d6cfedf71277666b802b4834565ee785bb
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-11-07 22:21:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,82 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const songRoutes = require('./routes/songs');
 const ejs = require('ejs');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://mongo:mongo@cluster0.6us6keo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+// MongoDB bağlantısı
+mongoose.connect('mongodb+srv://mongo:mongo@cluster0.6us6keo.mongodb.net/music-app?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB bağlantısı başarılı'))
   .catch(err => console.error('MongoDB bağlantı hatası:', err));
 
-
+// EJS ayarları
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Statik dosyalar (CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/songs', songRoutes);
+// URL kod çözme ve JSON isteği işleme
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// MongoDB Modeli - Song
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  }
+});
+
+const Song = mongoose.model('Song', songSchema);
+
+// Anasayfa - Tüm şarkıları listele
 app.get('/', async (req, res) => {
-  const Song = require('./models/song');
-  const songs = await Song.find({});
-  res.render('index', { songs });
+  try {
+    const songs = await Song.find({});
+    res.render('index', { songs });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Veritabanından şarkılar alınırken hata oluştu.');
+  }
 });
-app.get('/ses', async (req, res) => {
-  const Song = require('./models/song');
-  const songs = await Song.find({});
-  res.render('add', { songs });
+
+// Şarkı ekleme sayfası
+app.get('/ses', (req, res) => {
+  res.render('add');
 });
+
+// Şarkı ekleme işlemi
+app.post('/songs/add', async (req, res) => {
+  const { title, artist, genre } = req.body;
+
+  const newSong = new Song({
+    title,
+    artist,
+    genre
+  });
+
+  try {
+    await newSong.save();
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Şarkı eklenirken bir hata oluştu.');
+  }
+});
+
+// Sunucu başlatma
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
